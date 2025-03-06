9d321673a3e118c1abc57b4c7d590740b12f6466
Ramindu Jayaweera <ramindujayaweera2000@gmail.com>
2025-03-06 11:49:00 +0530

    sqlite3 connection

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./database.db",sqlite3.OPEN_READWRITE, (err)=>{
+    if(err) return console.error(err.message);
+
+    console.log("Connection Successful");
+})
