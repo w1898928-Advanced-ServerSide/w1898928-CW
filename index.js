a897a2e91faf778762207af0fd4046284eeee8f1
Ramindu Jayaweera <ramindujayaweera2000@gmail.com>
2025-04-16 00:50:19 +0530

    update ui

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const swaggerDocument = require('./swagger-output.json');
 const app = express();
 // Enable CORS for all routes
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 const PORT = process.env.PORT || 4000;
