13579ca5f4d2b57bd309da7cd27dab7fb171b39c
Ramindu Jayaweera <ramindujayaweera2000@gmail.com>
2025-04-15 13:28:14 +0530

    .github and docker file

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const authRoutes = require('./src/routes/authRoutes');
 const apiKeyRoutes = require('./src/routes/apiKeyRoutes');
 const countryRoutes = require('./src/routes/restCountryRoutes');
 const db = require('./src/config/db');
-const cors = require('cors'); // Add this line
+const cors = require('cors'); 
 const loggerMiddleware = require('./src/middleware/logger');
 const errorHandler = require('./src/middleware/errorHandler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 const app = express();
 // Enable CORS for all routes
@@ -31,6 +33,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/apiKey', apiKeyRoutes);
 app.use('/api/countries', countryRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandler)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Rest Country API',
+    description: 'API documentation for the Rest Country Backend'
+  },
+  host: 'localhost:3000', // Update to your production host if deployed
+  schemes: ['http'],      // Or ['https'] in production
+  
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./index.js']; // or your main entry file
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  console.log('✅ Swagger documentation generated successfully.');
+});
